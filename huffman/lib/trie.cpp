280e10954fc779168507ef706b5951f3b7abb93b
roman <romanfml31@gmail.com>
2019-06-07 23:55:51 +0300

    change to pointers

--- huffman/lib/trie.cpp
+++ huffman/lib/trie.cpp
@@ -1,54 +1,38 @@
 #include "trie.h"
 
-trie::trie() {
-    nv = 1;
-    add_node();
-}
-
-void trie::add_node() {
-    go.emplace_back(2, -1);
-    val.push_back(0);
-}
 
 trie::trie(const std::vector<std::pair<char, ull>>& codes) {
-    nv = 1;
-    add_node();
-
     for (uint i = 0; i < codes.size(); i++) {
         insert(codes[i].first, codes[i].second, i);
     }
 }
 
 void trie::insert(unsigned char len, ull code, unsigned char symbol) {
-    uint vert = 0;
+    node *vert = root;
     for (uint i = 0; i < len; i++) {
         uint cur = (code >> (len - i - 1u)) & 1u;
-        if (go[vert][cur] == -1) {
-            go[vert][cur] = nv++;
-            add_node();
+        node *son = (cur ? vert->R : vert->L);
+        if (!son) {
+            son = (cur ? vert->R : vert->L) = new node();
         }
-        vert = go[vert][cur];
+        vert = son;
     }
-    val[vert] = symbol;
+    vert->val = symbol;
 }
 
 bool trie::step(uint c) {
-    uint &cur = go[v][c];
-    if (cur != -1) {
-        v = cur;
-        return true;
-    }
-    return false;
+    v = (c ? v->R : v->L);
+    return (v != nullptr);
 }
 
 bool trie::end() const {
-    return go[v][0] == -1 && go[v][1] == -1;
+    return v->L == nullptr && v->R == nullptr;
 }
 
 unsigned char trie::get() const {
-    return val[v];
+    return v->val;
 }
 
 void trie::init() {
-    v = 0;
+    v = root;
 }
--- huffman/lib/trie.h
+++ huffman/lib/trie.h
@@ -7,22 +7,26 @@ typedef uint64_t ull;
 
 #include <vector>
 
+
 class trie {
 public:
     trie();
     explicit trie(const std::vector<std::pair<char, ull>>& codes);
 
-    int nv;
     bool step(uint c);
     bool end() const;
     unsigned char get() const;
     void init();
 private:
-    uint v = 0;
-    std::vector<unsigned char> val;
-    std::vector<std::vector<uint>> go;
+
+    struct node {
+        node *L = nullptr, *R = nullptr;
+        unsigned char val = 0;
+        node() = default;
+    };
+    node *root = new node();
+    node *v = root;
     void insert(unsigned char len, ull code, unsigned char symbol);
-    void add_node();
 };
 
 
